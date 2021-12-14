4b90a8783d11194fbe62315a4a1c477886cc691e
eemaharramli <91149686+eemaharramli@users.noreply.github.com>
2021-12-14 18:55:06 +0400

    Update index.js

--- first-task/js/index.js
+++ first-task/js/index.js
@@ -3,7 +3,7 @@ let boxes = document.querySelectorAll('.box')
 elements.forEach((item)=>{
     item.addEventListener('click',(e)=>{
         clearBox()
-        var color = e.target.getAttribute('data-color');
+        let color = e.target.getAttribute('data-color');
         document.getElementsByClassName(color)[0].style.backgroundColor = color;
     })
 })
