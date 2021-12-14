927f253b401ce6f289bd41acf77b1778453a3dbf
eemaharramli <91149686+eemaharramli@users.noreply.github.com>
2021-12-14 14:39:17 +0400

    Update index.js

--- first-task/js/index.js
+++ first-task/js/index.js
@@ -1,37 +1,15 @@
-// let buttons = document.querySelectorAll('.container__inner-btn')
-// let boxes = document.querySelectorAll('.box')
-// for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener("click", function(event) {
-//         if (boxes.getAttribute('class', event.target.innerHTML)){
-//             // boxes.style.backgroundColor = event.target.innerHTML
-//             console.log(event.target.innerHTML)
-//         }
-//     })
-//
-// }
-
-
-
 let elements = document.querySelectorAll('button');
 let boxes = document.querySelectorAll('.box')
 elements.forEach((item)=>{
-    item.addEventListener('click',()=>{
-        // console.log('item', item)
-        // console.log('item value', item.value)
-        // console.log('item inner', item.innerHTML.toLowerCase())
-        boxes.forEach((box)=>{
-            if (box.classList.contains(item.innerHTML.toLowerCase())){
-                clearBox()
-                box.style.backgroundColor = item.innerHTML.toLowerCase()
-            }
-        })
+    item.addEventListener('click',(e)=>{
+        clearBox()
+        var color = e.target.getAttribute('data-color');
+        document.getElementsByClassName(color)[0].style.backgroundColor = color;
     })
 })
-
 
 function clearBox() {
     boxes.forEach(item=>{
         item.style.backgroundColor = ''
     })
 }
-
